4858839ba7884136df0d66bccdd75eba20a0040f
Pramode <mail@pramode.in>
2017-11-30 00:04:00 +0530

    Remove the .closures folder

--- code/.closures/a.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let  t = |x, y| x + y;
-    println!("{}", t(1, 2));
-
-}
--- code/.closures/b.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let z = vec![1,2,3];
-    let  t = |x, y| x + y + z[0];
-    println!("{}", t(1, 2));
-
-}
